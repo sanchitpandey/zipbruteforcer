f4612be944d7c88aacb7591064b532483aebc629
sanchitpandey <58483403+sanchitpandey@users.noreply.github.com>
2019-12-05 21:00:39 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,21 @@
+import zipfile
+
+file = open("wordlist.txt","rb")
+count=1
+found = False
+for passb in file.readlines():
+    passb = passb.strip()
+    password = passb.decode('utf8')
+    x =  zipfile.ZipFile("Secret.zip","r")
+    try:
+        x.extractall(pwd=p)
+        print("Found correct password:",password)
+        found = True
+        break
+    except:
+        print("{}) {} is wrong".format(count,password))
+        count+=1
+
+if not found:
+    print("Password not in wordlist")
+        
